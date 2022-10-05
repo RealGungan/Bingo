30863777da36405e8474c8a91a6958c8fb827738
RealGungan <69304877+RealGungan@users.noreply.github.com>
2022-10-05 11:13:01 +0200

    Update bingo.php

--- bingo.php
+++ bingo.php
@@ -7,12 +7,11 @@
         table,
         tr,
         td {
-             border: 1px solid;
+            border: 1px solid;
             border-collapse: collapse;
-            font-size: 1.5rem;
+            font-size: 3rem;
             padding: 1rem;
-            width: 100px;
-            height: 50px;
+
             text-align: center;
         }
 
@@ -26,11 +25,18 @@
 
 <BODY>
     <?php
+    $ganador = false;
     printf("<div>");
-    createCarton();
-    createCarton();
-    createCarton();
+    $carton1 = createCarton();
+    $carton2 = createCarton();
+    $carton3 = createCarton();
     printf("</div>");
+
+    while (!$ganador) {
+        printf("<div>");
+        generarBolas($carton1, $carton2, $carton3);
+        printf("</div>");
+    }
 
     //función para generar arrays
     function createCarton()
@@ -40,6 +46,7 @@
         $numeros_rellenar = [];
         $numeros_rellenar_count = 0;
         $numeros_no_validos = [];
+        $rand = 0;
         $vacio = 0;
         $vacio_count = 0;
 
@@ -56,13 +63,23 @@
         for ($i = 0; $i < 3; $i++) {
             $arr_rows = [];
             printf("<tr>");
+            unset($numeros_no_validos);
+            $numeros_no_validos = [];
+            $numeros_rellenar = $numeros_bolsa;
             for ($j = 0; $j < 7; $j++) {
                 $vacio = rand(1, 10);
                 if ($vacio % 2 == 0 || $vacio_count == 6) {
                     $numeros_rellenar_count++;
-                    $numero = $numeros_rellenar[rand(1, 60 - $numeros_rellenar_count)];
-                    array_splice($numeros_rellenar, array_search($numero, $numeros_rellenar), 1);
+                    $rand = rand(1, 60 - $numeros_rellenar_count);
+                    while (array_search($rand, $numeros_no_validos)) {
+                        $rand = rand(1, 60 - $numeros_rellenar_count);
+                    }
+                    $numero = $numeros_rellenar[array_search($rand, $numeros_rellenar)];
+                    unset($numeros_rellenar[array_search($numero, $numeros_rellenar)]);
+                    $numeros_rellenar = array_values($numeros_rellenar);
+                    $numeros_no_validos[] = $numero;
                     printf("<td>" . $numero . "</td>");
+                    $arr_rows[] = $numero;
                 } else {
                     $arr_rows[] = "";
                     printf("<td></td>");
@@ -78,28 +95,44 @@
 
         return $arr_2d;
     }
+
     //FUNCION GENERAR BOLAS-------------------------------------------------------------------------
 
 
-function generarBolas(){
-$arrayBolas = array();
-$valorRandomPrimero = mt_rand(0,60);
-array_push($arrayBolas, $valorRandomPrimero);
-$x = 1;
-while ($x <= 60) {
-    $siguienteValorRadom = mt_rand(0, 60);
-    if(in_array($siguienteValorRadom, $arrayBolas)){
-        continue;
-    }else{
-    array_push($arrayBolas, $siguienteValorRadom);
-    $x++;
+    function generarBolas($carton1, $carton2, $carton3)
+    {
+        $arrayBolas = array();
+        $valorRandomPrimero = mt_rand(0, 60);
+        array_push($arrayBolas, $valorRandomPrimero);
+        $x = 1;
+        while ($x <= 60) {
+            $siguienteValorRadom = mt_rand(0, 60);
+            if (in_array($siguienteValorRadom, $arrayBolas)) {
+                continue;
+            } else {
+                array_push($arrayBolas, $siguienteValorRadom);
+                $x++;
+            }
+        }
+        $bola = array_pop($arrayBolas);
+        echo "Ha salido la bola :" . $bola;
+
+        comrpobar($bola, $carton1, $carton2, $carton3);
     }
-}
-print_r($arrayBolas);
-    
-$bola=array_pop($arrayBolas);
-echo "Ha salido la bola :" .$bola;
-}
+
+    // comprobar cartón
+    function comrpobar($bola, $carton1, $carton2, $carton3)
+    {
+        for ($i = 0; $i < 3; $i++) {
+            for ($j = 0; $j < 7; $j++) {
+                if ($bola == $carton1[$i][$j]) {
+                    $carton1[$i][$j] = true;
+                }
+            }
+        }
+    }
+
+
     ?>
 </BODY>
 
