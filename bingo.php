f07b61013cc9c007acfb31ee30df158acadf5b49
RealGungan <69304877+RealGungan@users.noreply.github.com>
2022-10-06 14:10:57 +0200

    Add files via upload

--- bingo.php
+++ bingo.php
@@ -9,40 +9,92 @@
         td {
             border: 1px solid;
             border-collapse: collapse;
-            font-size: 3rem;
-            padding: 1rem;
-
+            font-size: 1rem;
+            padding: .5rem;
+            width: 90px;
+            height: 40px;
             text-align: center;
+        }
+
+        div:nth-of-type(1),
+        div:nth-of-type(2),
+        div:nth-of-type(3),
+        div:nth-of-type(4) {
+            align-items: center;
         }
 
         div {
             width: fit-content;
             display: flex;
-            padding: 1rem
+            padding: .5rem;
         }
+
+        #bolas {
+            border: 1px solid black;
+            display: flex;
+            flex-wrap: wrap;
+            padding: .5rem;
+            float: left;
+        }
+
+        #bolasob {
+            width: 100%;
+            margin-left: 40%;
+        }
+
+        #image {
+            flex: 1 1 160px;
+        }
+
+        #ganador {}
     </style>
 </HEAD>
 
 <BODY>
     <?php
     $ganador = false;
-    printf("<div>");
-    $carton1 = createCarton();
-    $carton2 = createCarton();
-    $carton3 = createCarton();
+    $numeros_bolsa = [];
+    $bola = 0;
+
+    //generar números
+    for ($i = 0; $i < 60; $i++) {
+        $numeros_bolsa[$i] = $i + 1;
+    }
+
+    printf("<div><h3>JUGADOR 1</h3>");
+    $carton1 = createCarton($numeros_bolsa);
+    $carton2 = createCarton($numeros_bolsa);
+    $carton3 = createCarton($numeros_bolsa);
+    printf("</div>");
+
+    printf("<div><h3>JUGADOR 2</h3>");
+    $carton4 = createCarton($numeros_bolsa);
+    $carton5 = createCarton($numeros_bolsa);
+    $carton6 = createCarton($numeros_bolsa);
+    printf("</div>");
+
+    printf("<div><h3>JUGADOR 3</h3>");
+    $carton7 = createCarton($numeros_bolsa);
+    $carton8 = createCarton($numeros_bolsa);
+    $carton9 = createCarton($numeros_bolsa);
+    printf("</div>");
+
+    printf("<div><h3>JUGADOR 4</h3>");
+    $carton10 = createCarton($numeros_bolsa);
+    $carton11 = createCarton($numeros_bolsa);
+    $carton12 = createCarton($numeros_bolsa);
     printf("</div>");
 
     while (!$ganador) {
         printf("<div>");
-        generarBolas($carton1, $carton2, $carton3);
+        $ganador = comprobar($numeros_bolsa, $carton1, $carton2, $carton3, $carton4, $carton5, $carton6, $carton7, $carton8, $carton10, $carton11, $carton12, $bola, $ganador);
         printf("</div>");
     }
 
     //función para generar arrays
-    function createCarton()
+    function createCarton($numeros_bolsa)
     {
         $arr_2d = [];
-        $numeros_bolsa = [];
         $numeros_rellenar = [];
         $numeros_rellenar_count = 0;
         $numeros_no_validos = [];
@@ -52,11 +104,6 @@
 
         printf("<div>");
         printf("<table>");
-
-        //generar números
-        for ($i = 1; $i <= 60; $i++) {
-            $numeros_bolsa[$i] = $i;
-        }
 
         $numeros_rellenar = $numeros_bolsa;
 
@@ -96,42 +143,63 @@
         return $arr_2d;
     }
 
-    //FUNCION GENERAR BOLAS-------------------------------------------------------------------------
-
-
-    function generarBolas($carton1, $carton2, $carton3)
-    {
-        $arrayBolas = array();
-        $valorRandomPrimero = mt_rand(0, 60);
-        array_push($arrayBolas, $valorRandomPrimero);
-        $x = 1;
-        while ($x <= 60) {
-            $siguienteValorRadom = mt_rand(0, 60);
-            if (in_array($siguienteValorRadom, $arrayBolas)) {
-                continue;
-            } else {
-                array_push($arrayBolas, $siguienteValorRadom);
-                $x++;
-            }
-        }
-        $bola = array_pop($arrayBolas);
-        echo "Ha salido la bola :" . $bola;
-
-        comrpobar($bola, $carton1, $carton2, $carton3);
-    }
-
     // comprobar cartón
-    function comrpobar($bola, $carton1, $carton2, $carton3)
+    function comprobar($numeros_bolsa, $carton1, $carton2, $carton3, $carton4, $carton5, $carton6, $carton7, $carton8, $carton10, $carton11, $carton12, $bola, $ganador)
     {
-        for ($i = 0; $i < 3; $i++) {
-            for ($j = 0; $j < 7; $j++) {
-                if ($bola == $carton1[$i][$j]) {
-                    $carton1[$i][$j] = true;
+        $contador1 = 0;
+        $contador2 = 0;
+        $contador3 = 0;
+        $numeros_no_validos = [];
+        printf("<div id=\"bolas\"><div id=\"bolasob\"><h1>Bolas obtenidas</h1></div>");
+
+        while (!$ganador) {
+            $bola = rand(1, 60);
+            unset($numeros_bolsa[array_search($bola, $numeros_bolsa)]);
+            $numeros_bolsa = array_values($numeros_bolsa);
+
+            $numeros_no_validos[] = $bola;
+
+            printf("<div>Bola: <img src=images/" . $bola . ".png width=\"35\" height=\"35\"\/ id = \"image\"></div>");
+
+            for ($i = 0; $i < 3; $i++) {
+                for ($j = 0; $j < 7; $j++) {
+                    if ($carton1[$i][$j] == $bola) {
+                        $contador1++;
+                        if ($contador1 == 15) {
+                            printf("</div>");
+                            printf("<div id = \"ganador\">");
+                            printf("<br><br>El ganador es el jugador 1");
+                            printf("</div>");
+                            $ganador = true;
+                            return $ganador;
+                        }
+                    }
+                    if ($carton2[$i][$j] == $bola) {
+                        $contador2++;
+                        if ($contador2 == 15) {
+                            printf("</div>");
+                            printf("<div id = \"ganador\">");
+                            printf("<br><br>El ganador es el jugador 2");
+                            printf("</div>");
+                            $ganador = true;
+                            return $ganador;
+                        }
+                    }
+                    if ($carton3[$i][$j] == $bola) {
+                        $contador3++;
+                        if ($contador3 == 15) {
+                            printf("</div>");
+                            printf("<div id = \"ganador\">");
+                            printf("<br><br>El ganador es el jugador 3");
+                            printf("</div>");
+                            $ganador = true;
+                            return $ganador;
+                        }
+                    }
                 }
             }
         }
     }
-
 
     ?>
 </BODY>
